4a8a8eaf37baf5902c34f12681f17b931aed6c56
Oli <oli@hoodieboi.dev>
2022-12-26 05:59:53 +0100

    Remove velocity from player state for being too glitchy

--- src/main/java/lunarfreecam/freecam/PlayerState.java
+++ src/main/java/lunarfreecam/freecam/PlayerState.java
@@ -2,24 +2,20 @@ package lunarfreecam.freecam;
 
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 public class PlayerState {
 
     private final GameMode gameMode;
     private final float fallDistance;
-    private final Vector velocity;
 
     public PlayerState(@NotNull Player player) {
         gameMode = player.getGameMode();
         fallDistance = player.getFallDistance();
-        velocity = player.getVelocity();
     }
 
     public void apply(@NotNull Player player) {
         player.setGameMode(gameMode);
         player.setFallDistance(fallDistance);
-        player.setVelocity(velocity);
     }
 }
