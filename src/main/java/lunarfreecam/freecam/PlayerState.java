33a506cac80e6cfdf0493e25b317d92561365569
Oli <oli@hoodieboi.dev>
2022-12-26 06:43:56 +0100

    Track fire ticks

--- src/main/java/lunarfreecam/freecam/PlayerState.java
+++ src/main/java/lunarfreecam/freecam/PlayerState.java
@@ -8,14 +8,17 @@ public class PlayerState {
 
     private final GameMode gameMode;
     private final float fallDistance;
+    private final int fireTicks;
 
     public PlayerState(@NotNull Player player) {
         gameMode = player.getGameMode();
         fallDistance = player.getFallDistance();
+        fireTicks = player.getFireTicks();
     }
 
     public void apply(@NotNull Player player) {
         player.setGameMode(gameMode);
         player.setFallDistance(fallDistance);
+        player.setFireTicks(fireTicks);
     }
 }
